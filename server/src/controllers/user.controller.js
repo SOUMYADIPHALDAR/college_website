4999dbbef312249165d85b6aef3cfc33774e3fca
Soumya <soumyadiphaldar3@gmail.com>
2025-11-19 22:42:12 +0530

    add new features

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,6 +3,17 @@ import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js";
 import User from "../models/user.model.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: true });
+
+    return { accessToken, refreshToken };
+}
+
 const signUp = asyncHandler(async(req, res) => {
     const { fullName, email, registrationNumber, role, password } = req.body;
 
@@ -37,6 +48,46 @@ const signUp = asyncHandler(async(req, res) => {
     )
 });
 
+const logIn = asyncHandler(async(req, res) => {
+    const { email, registrationNumber, password } = req.b;
+
+    if (!email && !registrationNumber) {
+        throw new apiError(400, "email or registratio number is required..");
+    }
+
+    const checkUser = await User.findOne({ $or: [{email}, {registrationNumber}]});
+    if (!checkUser) {
+        throw new apiError(400, "You have to signed up first..");
+    }
+
+    const checkPassword = await checkUser.isPasswordCorrect(password);
+    if (checkPassword === true) {
+        throw new apiError(400, "Incorrect password..");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(checkUser._id);
+
+    const loggedInUser = await User.findById(checkUser._id).select("-password, -refreshToken");
+
+    const option = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, option)
+    .cookie("refreshToken", refreshToken, option)
+    .json(
+        new apiResponse(
+            200,
+            { user: loggedInUser, accessToken, refreshToken },
+            "Logged in successfully.."
+        )
+    )
+});
+
 export {
     signUp,
+    logIn
 }
